7a5ce1b718d6934e98720a8073aa282e6fec0be4
OctoJulo <octojulo@gmail.com>
2020-05-03 14:14:28 +0200

    modif key

--- footkey.ino
+++ footkey.ino
@@ -3,14 +3,13 @@
 // ***************************** Projet *************************** //
 // **************************************************************** //
 //////////////////////////////////////////////////////////////////////
-/*                     description             */
-/*                    description             */
+/*	controleur clavier à pied										*/
 /* **************************************************************** */
-/*  Version: 0.1        ---       Date: 00/00/0000  */  
-/*  OctoGeeK          ---           OctoJulo  */
+/*  Version: 0.1					---       		Date: 03/05/2020*/  
+/*  OctoGeeK						---           			OctoJulo*/
 /* **************************************************************** */
-/*  REQUIS:   - teensy LC                     */
-/*        - une matrice de bp 2*4               */
+/*  REQUIS:		- teensy LC                     					*/
+/*				- une matrice de bp 2*4               				*/
 /* **************************************************************** */
 //////////////////////////////////////////////////////////////////////
 // **************************************************************** //
@@ -24,20 +23,17 @@
 // ********** Constantes, Variables, Fonctions Internes *********** //
 // **************************************************************** //
 //////////////////////////////////////////////////////////////////////
-const byte ROWS = 2; // rows
-const byte COLS = 4; // columns
+const byte ROWS = 2; 												// nombre de ligne
+const byte COLS = 4; 												// nombre de colonne
 
-char keys[ROWS][COLS] = {
-	{KEY_ESC,KEY_A,KEY_E,KEY_PAGE_DOWN},
+char keys[ROWS][COLS] = {											// c'est ici que l'on place les commandes que l'on veut lier au bp.
+	{KEY_ESC,KEY_A,KEY_E,KEY_R},							// les valeurs sont trouvable sur le site: https://www.pjrc.com/teensy/td_keyboard.html
 	{KEY_LEFT_SHIFT,KEY_TAB,KEY_SPACE,KEY_ENTER}  
 };
+byte rowPins[ROWS] = {7,8};											// pins utilisées pour les lignes
+byte colPins[COLS] = {9,10,11,12};									// pins utilisées pour les colonnes 
 
-byte rowPins[ROWS] = {7,8}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9,10,11,12}; //connect to the column pinouts of the keypad
-
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-
-
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); // affecte à chaque ligne et colonne les commandes voulu avec la lib Keypad
 //////////////////////////////////////////////////////////////////////
 // **************************************************************** //
 // *********************** Progamme Principal ********************* //
@@ -45,7 +41,6 @@ Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 //////////////////////////////////////////////////////////////////////
 void setup()
 {
-
 } 
 //////////////////////////////////////////////////////////////////////
 // **************************************************************** //
@@ -56,13 +51,13 @@ void loop()
 {
 	if (kpd.getKeys())
 	{
-		for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+		for (int i=0; i<LIST_MAX; i++)   							// Scan the whole key list.
 		{
-			if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+			if ( kpd.key[i].stateChanged )  						// rentre dans la boucle si il y a changement d'etat.
 			{
-				switch (kpd.key[i].kstate) 
+				switch (kpd.key[i].kstate) 							// renvoi une valeur si pressed,hold,released
 				{
-					case PRESSED:
+					case PRESSED:	
 						Keyboard.set_key1(kpd.key[i].kchar);
 						Keyboard.send_now();
 						if (kpd.key[i].kchar == char(KEY_LEFT_SHIFT)) 
